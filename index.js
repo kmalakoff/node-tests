cbe3f2757f5ed488613c5c1f6f2dc9fc23ff7c04
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-29 16:02:01 -0700

    Got the tests to run

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var fs = require('fs');
-// var compat = {
-//   lstat: require('./lib/lstat'),
-//   readdir: require('./lib/readdir'),
-//   realpath: require('fs.realpath'),
-//   stat: require('./lib/stat'),
-// };
+var compat = {
+  lstat: require('./lib/lstat'),
+  readdir: require('./lib/readdir'),
+  realpath: require('fs.realpath'),
+  stat: require('./lib/stat'),
+};
 
-// for (var key in fs) {
-//   if (!compat[key]) compat[key] = fs[key];
-// }
+for (var key in fs) {
+  if (!compat[key]) compat[key] = fs[key];
+}
 
-// module.exports = compat;
-module.exports = fs;
+module.exports = compat;
