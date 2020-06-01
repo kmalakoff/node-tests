cbff66c67eef0c6f1ea315dfc7cfb21efb23f56b
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-31 17:52:30 -0700

    Added safe exit

--- bin/node-test.js
+++ bin/node-test.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var getopts = require('getopts-compat');
+var exit = require('exit');
+var NodeTests = require('..');
 
 (function () {
   var options = getopts(process.argv.slice(2), {
@@ -13,16 +15,15 @@ var getopts = require('getopts-compat');
   var args = options._;
   if (args.length < 1) {
     console.log('Missing command. Example usage: run-test [test]');
-    return process.exit(-1);
+    return exit(-1);
   }
-
-  var NodeTests = require('..');
 
   var tests = new NodeTests(options);
   tests.runTest(args[0], function (err) {
     if (err) {
-      console.log(err);
-      return process.exit(-1);
+      console.log(err.message);
+      return exit(err.code || -1);
     }
+    exit(0);
   });
 })();
